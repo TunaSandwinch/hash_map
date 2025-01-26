52d2cb0a8aa8bf17d0a103a80e7325166b8ed987
TunaSandwinch <winch.mascarenas@gmail.com>
2025-01-26 15:55:00 +0800

    refactor(manage_collision) : replace previous logic with key_index method for readability

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -19,20 +19,25 @@ class HashMap # rubocop:disable Style/Documentation
     end
   end
 
+  def get(key)
+    address = buckets[position(key)]
+    return unless address.length == 2
+
+    nil
+  end
+
   private
 
-  def manage_collisions(key, value) # rubocop:disable Metrics/MethodLength
+  def manage_collisions(key, value)
     address = buckets[position(key)]
-    current_index = 0
-    while current_index < address.length - 1
-      if  hash_code(key) == hash_code(address[current_index])
-        address[current_index + 1] = value
-        return
-      end
-      current_index += 2
+    key_position = key_index(key, address)
+
+    if key_position.nil?
+      address << key
+      address << value
+    else
+      address[key_position] = value
     end
-    address << key
-    address << value
   end
 
   def hash_code(key)
@@ -44,6 +49,7 @@ class HashMap # rubocop:disable Style/Documentation
     hash_code
   end
 
+  # gets the address(index) of the key in the hashmap
   def position(key)
     index = hash_code(key) % buckets.length
     raise IndexError if index.negative? || index >= @buckets.length
@@ -58,5 +64,15 @@ class HashMap # rubocop:disable Style/Documentation
 
   def update_buckets
     @buckets = buckets + Array.new(buckets.length) { [] }
+  end
+
+  def key_index(key, address)
+    current_index = 0
+    while current_index < address.length - 1
+      return current_index if hash_code(key) == hash_code(address[current_index])
+
+      current_index += 2
+    end
+    nil
   end
 end
